ffc74b2ff3f9c6625c9a092a74e8f9ab8a55a3a3
Asritha <asritha102001@gmail.com>
2026-08-13 21:32:03 +0530

    Developing stage orders

---
+++ models/Staging/stg_orders.sql
@@ -0,0 +1,20 @@
+select 
+--from raw_orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice-o.ordercostprice as orderprofit,
+--from raw_customers
+c.customername,
+c.country,
+c.segment,
+--from raw_products
+p.productname,
+p.category,
+p.subcategory
+from {{ref('RAW_ORDERS')}} as o
+left join {{ref('RAW_CUSTOMERS')}} as c
+on o.customerid=c.customerid
+LEFT JOIN {{ref('RAW_PRODUCTS')}} as p
+on o.productid=p.productid
